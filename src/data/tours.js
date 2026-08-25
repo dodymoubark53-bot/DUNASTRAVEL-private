be4e0d00ae639baa4f59455617ab7ad858e91296
eslam hefny <dodymoubark53-bot>
2026-08-25 14:31:29 +0300

    fix(greece): remove static Greece tour duplicate from frontend so it relies solely on backend API

--- src/data/tours.js
+++ src/data/tours.js
@@ -606,8 +606,7 @@ const baseTours = [
 
 import { tunisiaTours } from './tunisiaTours.js';
 import { moroccoTours } from './moroccoTours.js';
-import { greeceTours } from './greeceTours.js';
 
-export const tours = [...baseTours, ...tunisiaTours, ...moroccoTours, ...greeceTours];
+export const tours = [...baseTours, ...tunisiaTours, ...moroccoTours];
 export const allTours = tours;
 export default tours;
